3d10d5aecdc3e2e3c65f41ec26bce4fe7a8981b3
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-03-22 15:48:29 +0000

    Refactored my code as I wasn’t happy with it (mainly JS and CSS)

--- app.js
+++ app.js
@@ -1,194 +1,89 @@
 
-// Select all sections
-const formSections = document.querySelectorAll('.stepperForm__section');
-
-// Current section
-// This will be use to keep that on what section we need to show
-let currentSection = 1;
-
-
-/* Validate Function------------------------------------------------*/
+/* Validate  -------------------------------------------------------*/
 /*------------------------------------------------------------------*/
 /*------------------------------------------------------------------*/
+//  const validate = (element) => {
 
-// This Function checks if a section on the form has been filled in correctly
-const validate = (item) => {
-    // get input
-    const itemInput = item.querySelector('.stepperForm__input');
+//     // Get Icon
+//     const icon = element.parentElement.parentElement.querySelector('.icon');
 
-    // Get Label
-    const itemLable = item.querySelector('.stepperForm__label');
+//     // If element is not a submit run valibation
+//     if(!element.classList.contains('submit-btn')){
 
-        // If input is email validate it
-        if(itemInput.value === ""){
-
-            // Update label styles
-            itemLable.classList.add('label-error');
-            itemLable.classList.remove('label-pass');
-
-
-            // Update input styles
-            itemInput.classList.add('input-error');
-            itemInput.classList.remove('input-pass');
-
-            return false;
-        }else{
-            // Update label styles
-            itemLable.classList.remove('label-error');
-            itemLable.classList.add('label-pass');
-
-
-            // Update input styles
-            itemInput.classList.add('input-pass');
-            itemInput.classList.remove('input-error');
-
-            return true;
-        }
-       
-
-};
-
-
-/* Change Icon-------------------------------------------------------*/
-/*------------------------------------------------------------------*/
-/*------------------------------------------------------------------*/
-const updateIcon = (item) => {
-
-    // Get Icon Class
-    const targetIcon = item.querySelector('.icon');
-
-    // Add class
-    targetIcon.classList.add('icon-complete');
-
-    // Change Icon
-    targetIcon.innerHTML = '<i class="fas fa-check"></i>';
-
-
-};
-
-
-
-/* Move To Next Section Function------------------------------------*/
-/*------------------------------------------------------------------*/
-/*------------------------------------------------------------------*/
-
-const moveToNextSection = () => {
-
-    let counter = 0;
-    currentSection ++;
-
-    // loop trough all sections
-    formSections.forEach(formSection => {
-
-        // Remove active class
-        formSection.classList.remove('active');
-
-        // Add inactive class
-        formSection.classList.add('inactive');
-
-        // Add one to counter but only if less than the lenght of the about of form Sections (this is just in case we and more form sections in future)
-        if( counter < formSections.length){
-            counter ++;
-        }
+//          // Get input value
+//         const input = element.parentElement.querySelector('.stepperForm__input');
         
-        
+//         // Check if input has been filled in by the user
+//         if (input.value.trim() === ""){
+//             input.classList.add('validation-error');
+//             return false;
+//         }else{
+//             input.classList.remove('validation-error');
+//             icon.innerHTML = '<i class="fas fa-check"></i>';
+//             return true;
+//         }
+//     }
+//  };
 
 
-        // This will give use are new current section
-        if(counter === currentSection){
-            formSection.classList.add('active');
-            formSection.classList.remove('inactive');
-        }
 
-    });
-};
-
-
-/* Continue Buttons Event listeners --------------------------------*/
+/* Go Back to last section  ----------------------------------------*/
 /*------------------------------------------------------------------*/
 /*------------------------------------------------------------------*/
+// This function hides or shows a section depending on whether a continue or back button has been clicked 
+
+const changeSection = (buttonThatWasClicked) => {
 
 
-const continueButtons = document.querySelectorAll('.continue-btn').forEach(button => {
+    // Get the button's (the button passed in) Parent
+    const buttonsParent = buttonThatWasClicked.parentElement;
 
-    // Add an event listener to all continue-btn
-    button.addEventListener('click', () => {
+    // Get type for button
+    const typeOfButton = buttonThatWasClicked.classList;
 
-        // Get Parent Div
-        const section = button.parentElement.parentElement;
+    // Get all the formSections
+    const formSections = document.querySelectorAll('.stepperForm__sectionInputs');
 
-        // If the parentElement has the active class and it not the last section
-        if(section.classList.contains('active')){
+    // Convert formSections  NodeList to an array
+    const formSectionsArr = Array.from(formSections);
 
-
-            if(button.classList.contains('submit')){ // If Button is the submit just update the icon
-
-                 // Update Sections Icon
-                 updateIcon(section);
-
-            }else if(validate(section)){ // Run vailadtion an check if its passed 
-
-                // Update Sections Icon
-                updateIcon(section);
-
-                // Move to next section
-                moveToNextSection();
-            }
-
-        }
-    });
-});
+    // Loop over the array 
+     formSectionsArr.forEach((section) => {
 
 
-/* Go back to last Section Function---------------------------------*/
-/*------------------------------------------------------------------*/
-/*------------------------------------------------------------------*/
+        // Remove current classes
+        section.classList.remove('stepperForm__sectionInputs--show');
+        section.classList.remove('stepperForm__sectionInputs--hide');
 
-const goBackToLastSection = () => {
 
-    let counter = 0;
-    currentSection --;
+         // If typeOfButton is continue and the indexOf the current section is equal to the index of the buttonsParent +1, show that section
+        if(typeOfButton.contains('continue-btn') && formSectionsArr.indexOf(section) === formSectionsArr.indexOf(buttonsParent)+1){
+            section.classList.add('stepperForm__sectionInputs--show');
 
-     // loop trough all sections
-     formSections.forEach(formSection => {
-
-        console.log(currentSection);
-
-        // Remove active class
-        formSection.classList.remove('active');
-
-        // Add inactive class
-        formSection.classList.add('inactive');
-
-        // Add one to counter but only if less than the lenght of the about of form Sections (this is just in case we and more form sections in future)
-        if( counter < formSections.length){
-            counter ++;
-        }
-
-        // This will give use are new current section
-           if(counter === currentSection){
-            formSection.classList.add('active');
-            formSection.classList.remove('inactive');
+        // Else if typeOfButton is back and the indexOf the current section is equal to the index of the buttonsParent -1, show that section
+        } else if(typeOfButton.contains('back-btn') && formSectionsArr.indexOf(section) === formSectionsArr.indexOf(buttonsParent)-1){
+            section.classList.add('stepperForm__sectionInputs--show');
+        }else {
+             // Hide that section
+            section.classList.add('stepperForm__sectionInputs--hide');
         }
     });
 };
 
 
 
-/* Go Back Buttons Event listeners ---------------------------------*/
+/* Add Event Listeners to all submit buttons  ----------------------*/
 /*------------------------------------------------------------------*/
 /*------------------------------------------------------------------*/
 
+// Get form buttons
+const formButtons = document.querySelectorAll('.stepperForm__button ');
 
-const backButtons = document.querySelectorAll('.back-btn').forEach(button => {
-    // Add an event listener to all back-btn
-    button.addEventListener('click', () => {
-        // Get Parent Div
-        const section = button.parentElement.parentElement;
-
-        // If the parentElement has the active class and it not the last section
-        if(section.classList.contains('active')){
-            goBackToLastSection();
-        }
+// Add event Listener to all of the form buttons 
+formButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        // Run buttonClicked function and pass in the button that was clicked
+        //buttonClicked(button);
+        changeSection(button);
     });
 });
